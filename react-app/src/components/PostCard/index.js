3208802f1f4496074b75dd424901fd0fa4014378
MorganGravelat <mgravelat@gmail.com>
2022-04-19 20:11:45 -0400

    Fixed big bug with spread post links

--- react-app/src/components/PostCard/index.js
+++ react-app/src/components/PostCard/index.js
@@ -9,15 +9,15 @@ const PostCard = ({post}) => {
                 <h1>👎</h1>
             </div>
             <div className="post-detail-div">
-              <NavLink  className="post-title"exact to={`/posts/${post.post_user_id ? post?.post_user_id : post?.id}`}>
+              <NavLink  className="post-title"exact to={`/posts/${post.post_id ? post?.post_id : post?.id}`}>
                 <h3 style={{margin: "10px 0px 0px 0px"}}>{post.title ? post?.title : post?.post_title}</h3>
               </NavLink>
-              <NavLink className="post-description" exact to={`/posts/${post.post_user_id ? post?.post_user_id : post?.id}`}>
+              <NavLink className="post-description" exact to={`/posts/${post.post_id ? post?.post_id : post?.id}`}>
                 {post.post ? post?.post : post?.post_post}
               </NavLink>
             </div>
             <div className='space-between' />
-              <NavLink className="post-image-container" exact to={`/posts/${post.post_user_id ? post?.post_user_id : post?.id}`}>
+              <NavLink className="post-image-container" exact to={`/posts/${post.post_id ? post?.post_id : post?.id}`}>
                 <img className="post-image" src={post.image_url ? post?.image_url : post?.post_image_url} alt="post" />
               </NavLink>
         </div>
